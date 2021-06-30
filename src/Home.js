319d392c469fe260f9fe5ab8739739bedc64cbae
wqd182212 <wqd182212@163.com>
2021-06-30 18:34:04 +0800

    清除effect

--- src/Home.js
+++ src/Home.js
@@ -10,14 +10,21 @@ function Home() {
     }, [count]); // 仅在count更改时更新
 
     useEffect(() => {
-        console.log("wqd");
+        console.log('wqd');
     }, []); // 仅在组件挂载和卸载时执行
+
+    useEffect(() => {
+        let a = setInterval(() => console.log('a'), 5000);
+        return () => {
+            // 清除
+            clearInterval(a);
+        }
+    });
 
     return (
         <div>
-            <p>{ count }</p>
-            <Button onClick={() => setCount(count + 1)}>
-                click
+            <Button onClick={() => setCount(count => count + 1)} style={{ margin: 10 }}>
+                {count}
             </Button>
         </div>
     );
